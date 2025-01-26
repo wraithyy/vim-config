21c49b72308e72ca601263fd74718639e1e7bc44
Josef Kvapil <jkvapil@thetrask.com>
2025-01-26 18:31:38 +0100

    readme

--- init.lua
+++ init.lua
@@ -1,4 +1,12 @@
+_G.dd = function(...)
+	Snacks.debug.inspect(...)
+end
+_G.bt = function()
+	Snacks.debug.backtrace()
+end
+vim.print = _G.dd
 print("Ahoj Wraithy")
+
 require("config.lazy")
 require("config.highlight_on_yank")
 require("config.autoroot")
@@ -15,11 +23,27 @@ vim.opt.ignorecase = true
 vim.cmd([[
   autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focus=false})
 ]])
+--- lsp do notifier
+vim.api.nvim_create_autocmd("LspProgress", {
+	---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+	callback = function(ev)
+		local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+		vim.notify(vim.lsp.status(), "info", {
+			id = "lsp_progress",
+			title = "LSP Progress",
+			opts = function(notif)
+				notif.icon = ev.data.params.value.kind == "end" and " "
+					or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+			end,
+		})
+	end,
+})
 -- vim.api.nvim_create_autocmd("VimEnter", {
 -- 	callback = function()
 -- 		require("snacks").dashboard()
 -- 	end,
 -- })
+
 vim.opt.guicursor = {
 	"n-v-c:block", -- Normal, visual, command-line: block cursor
 	"i-ci-ve:ver25", -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
